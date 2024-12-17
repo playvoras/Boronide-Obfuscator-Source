3f7994ceafcb22caee37fdcc31fc17fb31fbc6db
playvora <longliator.dev@gmail.com>
2024-12-17 13:13:23 +0700

    lol

--- src/vm/opcodes/VARARG.lua
+++ src/vm/opcodes/VARARG.lua
@@ -2,7 +2,7 @@ local A	= |A|;
 local B	= |B|;
 local Stk, Vars	= Stack, Vararg;
 
-Top = A - 1;
+Top = A + Varargsz - 1;
 
 for Idx = A, A + (B > 0 and B - 1 or Varargsz) do
     Stk[Idx]	= Vars[Idx - A];
